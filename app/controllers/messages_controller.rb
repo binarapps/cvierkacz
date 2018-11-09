345431bfc43ee18efbc3846002598c0fe75d7bd1
Piotrek Kochowicz <p.kochowicz@binarapps.com>
2018-11-09 15:33:37 +0100

    lesson 4 - views, forms, devise

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,34 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to @message
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    if @message.update(message_params)
+      redirect_to @message
+    else
+      render :edit
+    end
   end
 
   def show
@@ -11,5 +39,11 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
     @message.destroy
     redirect_to messages_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
